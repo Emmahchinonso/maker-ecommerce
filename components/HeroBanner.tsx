6aae60d4449d6c8032c96bfd91a0d8aadf618d49
Emmahchinonso <emmanuelchinons9@gmail.com>
2022-05-22 11:37:09 +0100

    create headerBanner component

--- components/HeroBanner.tsx
+++ components/HeroBanner.tsx
@@ -1,29 +1,38 @@
+import { useNextSanityImage } from "next-sanity-image";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { client, urlFor } from "../libs/client";
 import { Banner } from "../sanity_maker-ecommerce/schema";
 
 interface BannerExt extends Banner {}
 
 type BannerProps = {
-  bannerData: BannerExt | [];
+  bannerData: BannerExt;
 };
 
 const HeroBanner = ({ bannerData }: BannerProps) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">SMALL Text</p>
-        <h3>MID TEXT</h3>
+        <p className="beats-solo">{bannerData?.smallText}</p>
+        <h3>{bannerData.midText}</h3>
+        <h1>{bannerData.largeText1}</h1>
         {/*eslint-disable-next-line @next/next/no-img-element*/}
-        <img src="" alt="hoodies" className="hero-banner-image" />
+        <Image
+          src={urlFor(bannerData.image).url()}
+          alt="hoodies"
+          className="hero-banner-image"
+          layout="fill"
+          objectFit="contain"
+        />
         <div>
-          <Link href="/product/10">
-            <button type="button">Button Text</button>
+          <Link href={`/product/${bannerData._id}`}>
+            <button type="button">{bannerData.buttonText}</button>
           </Link>
           <div className="desc">
             <h5>Description</h5>
-            <p>DESCRIPTION</p>
+            <p>{bannerData.desc}</p>
           </div>
         </div>
       </div>
